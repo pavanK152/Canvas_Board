f7ad84a3060dadcb038c187ba5ae0c3f15fb2e73
PavanK152 <neikora0245@gmail.com>
2025-12-01 18:32:18 +0530

    eraser and clear btn completed

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cleanBtn = document.getElementById("cleaner");
 const downloadBtn = document.getElementById("downloadImg");
 
 const canvas = document.getElementById("canvas");
-canvas.height = 800;
+canvas.height = 400;
 canvas.width = 800;
 
 // this will give us obj of canvas which has property to write on canvas
@@ -15,8 +15,10 @@ const ctx = canvas.getContext("2d");
 
 ctx.lineWidth = 5;
 ctx.lineCap = "round";
-ctx.stroke = "#000000";
+ctx.strokeStyle = "#000000";
 let isDrawing = false;
+let currentTool = "pen";
+
 function startDraw(e) {
   isDrawing = true;
   ctx.beginPath();
@@ -24,14 +26,37 @@ function startDraw(e) {
 }
 function draw(e) {
   if (isDrawing == false) return;
+  ctx.strokeStyle = currentTool === "eraser" ? "#ffffff" : colorInput.value;
+  ctx.lineWidth = brushSize.value;
   ctx.lineTo(e.offsetX, e.offsetY);
   ctx.stroke();
   ctx.beginPath();
   ctx.moveTo(e.offsetX, e.offsetY);
 }
 function stopDraw(e) {
-  isDrawing == false;
+  isDrawing = false;
 }
+
+penBtn.addEventListener("click", function () {
+  currentTool = "pen";
+  eraseBtn.classList.add("activeBtn");
+  squareBtn.classList.remove("activeBtn");
+  penBtn.classList.remove("activeBtn");
+});
+eraseBtn.addEventListener("click", function () {
+  currentTool = "eraser";
+  penBtn.classList.remove("activeBtn");
+  squareBtn.classList.remove("activeBtn");
+  eraseBtn.classList.add("activeBtn");
+});
+cleanBtn.addEventListener("click", function () {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+downloadBtn.addEventListener("click", function () {
+  const link = ctx.toDataURL("image/png");
+});
+
 canvas.addEventListener("mousedown", startDraw);
 canvas.addEventListener("mousemove", draw);
 canvas.addEventListener("mouseup", stopDraw);
