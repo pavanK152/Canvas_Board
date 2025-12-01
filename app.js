5d26856aebdfbdaa96c8dab9a7453ec4ed66f874
PavanK152 <neikora0245@gmail.com>
2025-12-01 19:20:55 +0530

    squrebtn completed

--- app.js
+++ app.js
@@ -19,13 +19,22 @@ ctx.strokeStyle = "#000000";
 let isDrawing = false;
 let currentTool = "pen";
 
+let startX = 0;
+let startY = 0;
+
 function startDraw(e) {
   isDrawing = true;
+
+  if (currentTool == "square") {
+    startX = e.offsetX;
+    startY = e.offsetY;
+    return;
+  }
   ctx.beginPath();
   ctx.moveTo(e.offsetX, e.offsetY);
 }
 function draw(e) {
-  if (isDrawing == false) return;
+  if (isDrawing == false || currentTool == "square") return;
   ctx.strokeStyle = currentTool === "eraser" ? "#ffffff" : colorInput.value;
   ctx.lineWidth = brushSize.value;
   ctx.lineTo(e.offsetX, e.offsetY);
@@ -34,19 +43,29 @@ function draw(e) {
   ctx.moveTo(e.offsetX, e.offsetY);
 }
 function stopDraw(e) {
+  if (currentTool == "square") {
+    let endX = e.offsetX;
+    let endY = e.offsetY;
+    let width = endX - startX;
+    let height = endY - startY;
+    ctx.beginPath();
+    ctx.rect(startX, startY, width, height);
+    ctx.stroke();
+  }
   isDrawing = false;
 }
 
 penBtn.addEventListener("click", function () {
   currentTool = "pen";
-  eraseBtn.classList.add("activeBtn");
+  eraseBtn.classList.remove("activeBtn");
   squareBtn.classList.remove("activeBtn");
-  penBtn.classList.remove("activeBtn");
+  penBtn.classList.add("activeBtn");
 });
 eraseBtn.addEventListener("click", function () {
   currentTool = "eraser";
   penBtn.classList.remove("activeBtn");
   squareBtn.classList.remove("activeBtn");
+
   eraseBtn.classList.add("activeBtn");
 });
 cleanBtn.addEventListener("click", function () {
@@ -63,6 +82,12 @@ downloadBtn.addEventListener("click", function () {
   document.body.removeChild(anchorTag);
 });
 
+squareBtn.addEventListener("click", function () {
+  currentTool = "square";
+  penBtn.classList.remove("activeBtn");
+  eraseBtn.classList.remove("activeBtn");
+  squareBtn.classList.add("activeBtn");
+});
 canvas.addEventListener("mousedown", startDraw);
 canvas.addEventListener("mousemove", draw);
 canvas.addEventListener("mouseup", stopDraw);
